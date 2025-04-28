77e8d3f6a51857840148fda246d3ac23fc7da7cf
aw-jwalker <jwalker@assetwatch.com>
2025-04-28 01:02:03 -0400

    markdown preview still isnt working but its fine i guess

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -4,15 +4,28 @@ return {
   build = 'cd app && yarn install',
   init = function()
     vim.g.mkdp_filetypes = { 'markdown' }
-    vim.g.mkdp_browser = 'firefox'
+    vim.g.mkdp_browser = 'chrome'
+
+    -- Set up custom browser function for Ubuntu
+    vim.g.mkdp_browserfunc = 'g:Open_browser'
+
+    -- Define the function to open URLs in Chrome
+    --    vim.cmd [[
+    --      function! g:Open_browser(url) abort
+    --        execute "silent! !/usr/bin/google-chrome --new-window " . shellescape(a:url)
+    --      endfunction
+    --    ]]
+
+    vim.cmd [[
+      function! g:Open_browser(url) abort
+        execute "silent! !xdg-open " . shellescape(a:url) . " &"
+      endfunction
+    ]]
+
+    -- Additional settings for better experience
+    vim.g.mkdp_auto_close = 0 -- Don't close the preview when changing buffers
+    vim.g.mkdp_refresh_slow = 0 -- Refresh preview on save or leave insert mode
+    vim.g.mkdp_echo_preview_url = 1 -- Echo the preview URL in the command line
   end,
   ft = { 'markdown' },
 }
-
--- how do I add this function?
---
--- function g:open_browser(url) abort
---  " open url here
--- endfunction
-
--- let g:mkdp_browserfunc = 'g:open_browser'
